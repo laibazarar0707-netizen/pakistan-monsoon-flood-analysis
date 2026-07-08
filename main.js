f2efce553024cddcf5fb0c03c825c1c776d2812f
Laiba Zarar Noor <laibazarar0707@gmail.com>
2026-07-08 06:20:14 +0500

    Create main.js
    Master script that runs and coordinates the full 10-script pipeline, from data extraction through final export.

---
+++ main.js
@@ -0,0 +1,7 @@
+var viz = require('users/Noor_rr/Pak_Monsoon_2015_to_2025:07_visualization.js');
+var exp = require('users/Noor_rr/Pak_Monsoon_2015_to_2025:08_exports.js');
+
+viz.addAllLayersToMap();
+exp.runAllExports();
+
+print('Script loaded. Check Layers panel for maps, Tasks tab for 31 queued exports.');
